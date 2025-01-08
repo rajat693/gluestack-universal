207051f90c44cb4ce93c210916e64733ec176eb1
rajat693 <rajatchaudhary.rc693@gmail.com>
2025-01-08 11:20:04 +0530

    feat: created expo-router-app in apps folder

---
+++ apps/expo-router-app/app/_layout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="about" />
+    </Stack>
+  );
+}
